c60858354a9362cc3a03686820627b2bdc95293c
liradb2000 <liradb2000@live.co.kr>
2025-08-03 01:08:52 +0900

    chore: update webpack configuration to set alias for Tiptap components

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+const path = require("path");
 module.exports = (config, env) => {
+  config.resolve.alias = {
+    "@": path.resolve(__dirname, "./src/components/tiptap"),
+  };
+  config.resolve.extensions = [".js", ".jsx"];
   return config;
 };
